ce1f5337dc4b0c0a5c8c1fc9ccbcecaa6b771cbc
Tony Richards <trichards57@pm.me>
2023-11-16 07:16:45 +0000

    Added more detail to the VOR Status result.

--- VorReceiver/VorStatus.cs
+++ VorReceiver/VorStatus.cs
@@ -11,77 +11,138 @@ using System.Linq;
 using System.Threading.Tasks;
 using VorReceiver.Model;
 using Microsoft.Azure.Cosmos.Linq;
+using Newtonsoft.Json;
 
-namespace VorReceiver
+namespace VorReceiver;
+
+public class VorStatus
 {
-    public class VorStatus
-    {
-        private readonly CosmosClient cosmosClient;
-        private readonly IConfiguration configuration;
+    private readonly CosmosClient cosmosClient;
+    private readonly IConfiguration configuration;
 
-        public VorStatus(CosmosClient cosmosClient, IConfiguration configuration)
+    public VorStatus(CosmosClient cosmosClient, IConfiguration configuration)
+    {
+        this.cosmosClient = cosmosClient;
+        this.configuration = configuration;
+    }
+
+    [FunctionName("vor-status")]
+    public async Task<IActionResult> Run(
+    [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+    ILogger log)
+    {
+        double cost = 0;
+        log.LogInformation("Received request.");
+
+        var parameters = req.GetQueryParameterDictionary();
+        var callsignsValid = parameters.TryGetValue("callsigns", out var callsigns);
+
+        if (!callsignsValid || string.IsNullOrWhiteSpace(callsigns))
         {
-            this.cosmosClient = cosmosClient;
-            this.configuration = configuration;
+            log.LogError("No callsigns received.");
+
+            return new BadRequestObjectResult(new ProblemDetails()
+            {
+                Detail = "No callsigns received.",
+                Instance = req.Path,
+                Status = StatusCodes.Status400BadRequest,
+                Title = "No callsigns received.",
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+            });
         }
 
-        [FunctionName("vor-status")]
-        public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-        ILogger log)
+        var parts = callsigns.Split(",").Select(s => s.Trim().ToUpperInvariant());
+
+        log.LogInformation($"Received call-signs {string.Join(", ", parts)}.");
+
+        var container = cosmosClient.GetVorContainer(configuration);
+
+        var items = container.GetItemLinqQueryable<Vehicle>().Where(v => parts.Contains(v.CallSign.ToUpper().Trim())).ToFeedIterator();
+
+        var results = new Dictionary<string, VorStatusResult>();
+
+        while (items.HasMoreResults)
         {
-            double cost = 0;
-            log.LogInformation("Received request.");
+            var response = await items.ReadNextAsync();
+            cost += response.RequestCharge;
 
-            var parameters = req.GetQueryParameterDictionary();
-            var callsignsValid = parameters.TryGetValue("callsigns", out var callsigns);
-
-            if (!callsignsValid || string.IsNullOrWhiteSpace(callsigns))
+            foreach (var item in response)
             {
-                log.LogError("No callsigns received.");
-
-                return new BadRequestObjectResult(new ProblemDetails()
+                if (item.IsVor)
                 {
-                    Detail = "No callsigns received.",
-                    Instance = req.Path,
-                    Status = StatusCodes.Status400BadRequest,
-                    Title = "No callsigns received.",
-                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
-                });
-            }
+                    var incident = item.Incidents.OrderByDescending(s => s.StartDate).FirstOrDefault();
 
-            var parts = callsigns.Split(",").Select(s => s.Trim().ToUpperInvariant());
+                    results[item.CallSign.ToUpperInvariant().Trim()] = new VorStatusResult { 
+                        IsVor = item.IsVor,
+                        DueBack = incident?.EstimatedEndDate,
+                        Summary = incident?.Description,
+                    };
 
-            log.LogInformation($"Received call-signs {string.Join(", ", parts)}.");
-
-            var container = cosmosClient.GetVorContainer(configuration);
-
-            var items = container.GetItemLinqQueryable<Vehicle>().Where(v => parts.Contains(v.CallSign.ToUpper().Trim())).ToFeedIterator();
-
-            var results = new Dictionary<string, bool>();
-
-            while (items.HasMoreResults)
-            {
-                var response = await items.ReadNextAsync();
-                cost += response.RequestCharge;
-
-                foreach (var item in response)
+                }
+                else
                 {
-                    results[item.CallSign.ToUpperInvariant().Trim()] = item.IsVor;
+                    results[item.CallSign.ToUpperInvariant().Trim()] = new VorStatusResult { IsVor = false };
                 }
             }
+        }
 
-            foreach (var p in parts)
+        foreach (var p in parts)
+        {
+            if (!results.ContainsKey(p))
             {
-                if (!results.ContainsKey(p))
-                {
-                    results[p] = false;
-                }
+                results[p] = new VorStatusResult { IsVor = false };
+            }
+        }
+
+        log.LogInformation($"Request completed.  {cost} RUs expended.");
+
+        return new OkObjectResult(results);
+    }
+}
+
+public class VorStatusResultConverter : JsonConverter<VorStatusResult>
+{
+    public override VorStatusResult ReadJson(JsonReader reader, Type objectType, VorStatusResult existingValue, bool hasExistingValue, JsonSerializer serializer) => throw new NotImplementedException();
+
+    public override void WriteJson(JsonWriter writer, VorStatusResult value, JsonSerializer serializer)
+    {
+        if (!value.IsVor)
+        {
+            writer.WriteValue(false);
+        }
+        else
+        {
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("isVor");
+            writer.WriteValue(value.IsVor);
+
+            if (value.DueBack.HasValue)
+            {
+                writer.WritePropertyName("dueBack");
+                writer.WriteValue(value.DueBack);
             }
 
-            log.LogInformation($"Request completed.  {cost} RUs expended.");
+            if (!string.IsNullOrWhiteSpace(value.Summary))
+            {
+                writer.WritePropertyName("summary");
+                writer.WriteValue(value.Summary.Trim());
+            }
 
-            return new OkObjectResult(results);
+            writer.WriteEndObject();
         }
     }
+}
+
+
+public readonly record struct VorStatusResult
+{
+    [JsonProperty("isVor")]
+    public bool IsVor { get; init; }
+
+    [JsonProperty("dueBack", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+    public DateOnly? DueBack { get; init; }
+
+    [JsonProperty("summary", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+    public string Summary { get; init; }
 }
