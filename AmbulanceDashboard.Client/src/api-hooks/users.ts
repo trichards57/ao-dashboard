38229ac0dfecb0dec0c0bd1d89dfd0bb141bf1e1
Tony Richards <trichards57@gmail.com>
2024-03-01 11:37:32 +0000

    Merged PR 203: Finessing the Published Version
    - Set users query to tolerate not being logged in.
    - Set react-router dev tools to not render in prod.
    - Tidied up packages.
    - Improved caching.

--- AmbulanceDashboard.Client/src/api-hooks/users.ts
+++ AmbulanceDashboard.Client/src/api-hooks/users.ts
@@ -7,8 +7,7 @@
 // -----------------------------------------------------------------------
 
 import { useQuery } from "@tanstack/react-query";
-
-import useQueryFunction from "./query-fn";
+import { useAccount, useMsal } from "@azure/msal-react";
 
 interface IUserPermissions {
   userId: string;
@@ -21,13 +20,38 @@ interface IUserPermissions {
 
 // eslint-disable-next-line import/prefer-default-export
 export const useUserPermissions = () => {
-  const uri = "/api/user/permissions";
+  const { accounts, instance } = useMsal();
+  const account = useAccount(accounts[0] || {});
 
-  const permissionsQuery = useQueryFunction<IUserPermissions>(uri);
+  const uri = "/api/user/permissions";
 
   return useQuery({
     queryKey: ["users", "me"],
-    queryFn: permissionsQuery,
+    queryFn: async () => {
+      if (account) {
+        const authResponse = await instance.acquireTokenSilent({
+          account,
+          scopes: ["ae7dee55-3f98-4bda-b5cf-7641de4a1776/.default"],
+        });
+        const response = await fetch(uri, {
+          headers: { Authorization: `Bearer ${authResponse.accessToken}` },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        return await response.json() as IUserPermissions;
+      }
+
+      return {
+        userId: "",
+        canViewVehicles: false,
+        canEditVehicles: false,
+        canViewPlaces: false,
+        canEditVOR: false,
+        canViewVOR: false,
+      };
+    },
     throwOnError: true,
   });
 }
