62a11ad7a5bb88304fba5a867c46320b269b9bed
Tony Richards <trichards57@pm.me>
2024-04-24 08:44:34 +0100

    Finished updating the login page.

---
+++ Dashboard/Dashboard.Client/Components/BulmaFieldCssClassProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Dashboard.Client.Components;
+
+public class BulmaFieldCssClassProvider : FieldCssClassProvider
+{
+    public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+    {
+        var isValid = editContext.IsValid(fieldIdentifier);
+
+        if (!isValid)
+        {
+            return "is-danger";
+        }
+
+        if (editContext.IsModified(fieldIdentifier))
+        {
+            return "is-success";
+        }
+
+        return "";
+    }
+}
---
+++ Dashboard/Dashboard.Client/Validation/NonSjaEmailAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard.Client.Validation
+{
+    public class NonSjaEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string email && email.EndsWith("@sja.org.uk"))
+            {
+                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName), 
+                    validationContext.MemberName != null ? [validationContext.MemberName] : null);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
