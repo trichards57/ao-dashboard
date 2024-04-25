46907091c4be190f612feb459bf0533d602bcc68
Tony Richards <trichards57@pm.me>
2024-04-25 08:53:40 +0100

    Continued updating the UI.

---
+++ Dashboard/Dashboard.Client/Validation/BooleanRequiredAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard.Client.Validation;
+
+public class BooleanRequiredAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not bool b || !b)
+        {
+            return new ValidationResult(FormatErrorMessage(validationContext.DisplayName),
+                validationContext.MemberName != null ? [validationContext.MemberName] : null);
+        }
+
+        return ValidationResult.Success;
+    }
+}
