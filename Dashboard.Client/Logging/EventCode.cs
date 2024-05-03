be3a064d9abce8b116b92c4e98cd2f35f6d200a5
Tony Richards <trichards57@pm.me>
2024-05-03 17:38:07 +0100

    Refined the logging for the disable two-factor page.

--- Dashboard.Client/Logging/EventCode.cs
+++ Dashboard.Client/Logging/EventCode.cs
@@ -3,4 +3,5 @@
 public static class EventCode
 {
     public const int User_ChangedPassword = 1000;
+    public const int User_DisabledTwoFactor = 1001;
 }
--- Dashboard/Logging/LoggingMessages.cs
+++ Dashboard/Logging/LoggingMessages.cs
@@ -6,4 +6,7 @@ public static partial class LoggingMessages
 {
     [LoggerMessage(EventCode.User_ChangedPassword, LogLevel.Information, "User {id} has changed their password.")]
     public static partial void User_ChangedPassword(this ILogger logger, string id);
+
+    [LoggerMessage(EventCode.User_DisabledTwoFactor, LogLevel.Information, "User {id} has disabled two-factor authentication.")]
+    public static partial void User_DisabledTwoFactor(this ILogger logger, string id);
 }
