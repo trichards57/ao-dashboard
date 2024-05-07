5b6e562c1c9d65075ca08530d3cb0248c36234f4
Tony Richards <trichards57@pm.me>
2024-05-07 08:12:52 +0100

    Fixed some warnings.

--- VorUploader/Program.cs
+++ VorUploader/Program.cs
@@ -23,8 +23,24 @@ namespace VorUploader;
 /// </summary>
 internal static class Program
 {
-    const string InboxId = "AAMkAGU0NTA2ODczLTYyZWEtNGZhNS04MDQ0LTIwMDFlMDQzN2M0ZQAuAAAAAAB20NTeTHozS4sqw68MW0ExAQBWg0hGD4wFSaUCnLIUVcwOAAAA-DKkAAA=";
-    const string VorFolderId = "AAMkAGU0NTA2ODczLTYyZWEtNGZhNS04MDQ0LTIwMDFlMDQzN2M0ZQAuAAAAAAB20NTeTHozS4sqw68MW0ExAQD7P9wa3zxsSruC4g1eWMZTAATImwK9AAA=";
+    private const string InboxId = "AAMkAGU0NTA2ODczLTYyZWEtNGZhNS04MDQ0LTIwMDFlMDQzN2M0ZQAuAAAAAAB20NTeTHozS4sqw68MW0ExAQBWg0hGD4wFSaUCnLIUVcwOAAAA-DKkAAA=";
+    private const string VorFolderId = "AAMkAGU0NTA2ODczLTYyZWEtNGZhNS04MDQ0LTIwMDFlMDQzN2M0ZQAuAAAAAAB20NTeTHozS4sqw68MW0ExAQD7P9wa3zxsSruC4g1eWMZTAATImwK9AAA=";
+    private const string TokenReplyString = "http://localhost/";
+
+    private static async Task CancelToken(IServiceProvider provider, string token)
+    {
+        var service = provider.GetRequiredService<OpenIddictClientService>();
+
+        await service.RevokeTokenAsync(new() { Token = token });
+    }
+
+    private static async Task<string> GetTokenAsync(IServiceProvider provider)
+    {
+        var service = provider.GetRequiredService<OpenIddictClientService>();
+
+        var result = await service.AuthenticateWithClientCredentialsAsync(new());
+        return result.AccessToken;
+    }
 
     private static async Task Main()
     {
@@ -77,8 +93,8 @@ internal static class Program
         {
             TenantId = tenantId,
             ClientId = graphClientId,
-            RedirectUri = new Uri("http://localhost/"),
-            LoginHint = "tony.richards1@sja.org.uk"
+            RedirectUri = new Uri(TokenReplyString),
+            LoginHint = "tony.richards1@sja.org.uk",
         };
 
         var interactiveCredential = new InteractiveBrowserCredential(options);
@@ -89,7 +105,7 @@ internal static class Program
 
         if (emails?.Value != null)
         {
-            foreach (var e in emails.Value.Where(e => e.Subject?.Equals("Daily VOR Report", StringComparison.OrdinalIgnoreCase) == true)) 
+            foreach (var e in emails.Value.Where(e => e.Subject?.Equals("Daily VOR Report", StringComparison.OrdinalIgnoreCase) == true))
             {
                 var attachments = await graphClient.Me.Messages[e.Id].Attachments.GetAsync();
                 var attachment = attachments?.Value?.Find(a => a.Name?.EndsWith(".xls", StringComparison.OrdinalIgnoreCase) == true);
@@ -138,59 +154,6 @@ internal static class Program
             }
         }
 
-        //foreach (var file in Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.xls").OrderBy(f => f))
-        //{
-        //    Console.WriteLine($"Found File - {file}");
-
-        //    var fileDate = DateOnly.Parse(Path.GetFileNameWithoutExtension(file).Split(' ')[0], CultureInfo.CurrentCulture);
-
-        //    Console.WriteLine($"File Date  - {fileDate}");
-
-        //    var items = FileParser.ParseFile(file, fileDate);
-
-        //    var count = 0;
-        //    HttpResponseMessage result;
-
-        //    do
-        //    {
-        //        if (count > 0)
-        //        {
-        //            await Task.Delay(TimeSpan.FromSeconds(5));
-        //        }
-
-        //        result = await httpClient.PostAsJsonAsync(vorUris, items);
-        //        count++;
-        //    }
-        //    while (count < 3 && !result.IsSuccessStatusCode);
-
-        //    if (!result.IsSuccessStatusCode)
-        //    {
-        //        Console.WriteLine($"Received Error : {result.StatusCode}");
-        //    }
-        //    else
-        //    {
-        //        var dirPath = Path.GetDirectoryName(file) ?? Environment.CurrentDirectory;
-        //        Directory.CreateDirectory(Path.Combine(dirPath, "Uploaded"));
-        //        File.Move(file, Path.Combine(dirPath, "Uploaded", Path.GetFileName(file)), true);
-        //    }
-
-        //}
-
         await CancelToken(provider, token);
-    }
-
-    static async Task<string> GetTokenAsync(IServiceProvider provider)
-    {
-        var service = provider.GetRequiredService<OpenIddictClientService>();
-
-        var result = await service.AuthenticateWithClientCredentialsAsync(new());
-        return result.AccessToken;
-    }
-
-    static async Task CancelToken(IServiceProvider provider, string token)
-    {
-        var service = provider.GetRequiredService<OpenIddictClientService>();
-
-        await service.RevokeTokenAsync(new() { Token = token });
     }
 }
