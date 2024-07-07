9acb1eab530914f2d18137d5df15c452c5cc1ba6
Tony Richards <trichards57@pm.me>
2024-07-07 09:40:43 +0100

    Started vehicle setup page.

--- Dashboard2/dashboard2.client/src/queries/vehicle-queries.ts
+++ Dashboard2/dashboard2.client/src/queries/vehicle-queries.ts
@@ -1,1 +1,35 @@
+export interface VehicleSettings {
+  id: string;
+  registration: string;
+  callSign: string;
+  hub: string;
+  district: string;
+  region: string;
+  vehicleType: string;
+  forDisposal: boolean;
+}
 
+export const settingsOptions = (
+  region: string | undefined,
+  district: string | undefined,
+  hub: string | undefined,
+) => ({
+  queryKey: ["status", region ?? "All", district ?? "All", hub ?? "All"],
+  queryFn: async () => {
+    const response = await fetch(
+      `/api/vehicles?region=${region ?? "All"}&district=${district ?? "All"}&hub=${hub ?? "All"}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch vehicle settings.");
+    }
+
+    return response.json() as Promise<VehicleSettings[]>;
+  },
+});
--- Dashboard2/dashboard2.client/src/routes/vehicles/config.tsx
+++ Dashboard2/dashboard2.client/src/routes/vehicles/config.tsx
@@ -1,7 +1,78 @@
-import { createFileRoute } from '@tanstack/react-router'
-import validatePlace from '../../support/validate-place'
+import { createFileRoute } from "@tanstack/react-router";
+import validatePlace from "../../support/validate-place";
+import PlacePicker from "../../components/place-picker";
+import { settingsOptions } from "../../queries/vehicle-queries";
 
-export const Route = createFileRoute('/vehicles/config')({
+const VehicleConfig = () => {
+  return (
+    <>
+      <h1 className="title">Vehicle Setup</h1>
+      <PlacePicker />
+      <div className="table-container">
+        <table className="table is-striped is-fullwidth">
+          <thead>
+            <tr>
+              <th className="edit-col"></th>
+              <th className="call-sign-col">Call Sign</th>
+              <th className="reg-col">Registration</th>
+              <th>Region</th>
+              <th>District</th>
+              <th>Hub</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* if (StatusesToDisplay.Any())
+              { */}
+            {/* foreach (var s in StatusesToDisplay.Skip(page * PageSize).Take(PageSize))
+                  { */}
+            <tr>
+              <td className="edit-col">
+                <a href="/vehicles/config/@s.Id">
+                  <img className="fa-img" src="/img/fa/pen.svg" />
+                </a>
+              </td>
+              <td className="call-sign-col">@s.CallSign</td>
+              <td className="reg-col">@s.Registration</td>
+              <td className="place-col">
+                @RegionConverter.ToDisplayString(s.Region)
+              </td>
+              <td className="place-col">@s.District</td>
+              <td className="place-col">@s.Hub</td>
+            </tr>
+            {/* }
+              }
+              else
+              { */}
+            <tr>
+              <td colSpan={6} className="no-vehicles">
+                No Vehicles
+              </td>
+            </tr>
+            {/* } */}
+          </tbody>
+        </table>
+      </div>
+      {/* @if (ShowPagination)
+  {
+      <PagePicker Page="@(page)"
+                  Pages="@((int)Math.Ceiling((float)StatusesToDisplay.Count()/PageSize))"
+                  ChangePage="p => page = p" />
+  }) */}
+    </>
+  );
+};
+
+export const Route = createFileRoute("/vehicles/config")({
   validateSearch: validatePlace,
-  component: () => <div>Hello /vehicles/config!</div>
-})
+  loaderDeps: ({ search: { region, district, hub } }) => ({
+    region,
+    district,
+    hub,
+  }),
+  loader: ({ deps, context }) => {
+    return context.queryClient.ensureQueryData(
+      settingsOptions(deps.region, deps.district, deps.hub),
+    );
+  },
+  component: VehicleConfig,
+});
