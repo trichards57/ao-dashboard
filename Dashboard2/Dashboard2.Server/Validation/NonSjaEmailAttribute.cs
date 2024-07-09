bae963c6bac19b9f98682eec713c45316da5b97e
Tony Richards <trichards57@pm.me>
2024-07-09 12:18:34 +0100

    Update the manage email page.

---
+++ Dashboard2/Dashboard2.Server/Validation/NonSjaEmailAttribute.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NonSjaEmailAttribute.cs" company="Tony Richards">
+// Copyright (c) Tony Richards. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Dashboard.Client.Validation;
+
+/// <summary>
+/// A validation attribute to ensure that an email address is not an SJA email address.
+/// </summary>
+public class NonSjaEmailAttribute : ValidationAttribute
+{
+    /// <inheritdoc/>
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is string email && email.EndsWith("@sja.org.uk"))
+        {
+            return new ValidationResult(
+                FormatErrorMessage(validationContext.DisplayName),
+                validationContext.MemberName != null ? [validationContext.MemberName] : null);
+        }
+
+        return ValidationResult.Success;
+    }
+}
